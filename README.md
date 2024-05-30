df027cc928f5a876419b513c2629cfe0949e1b58
Rebeca Mello <rebecammsilva@outlook.com>
2024-05-30 20:20:06 -0300

    commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Aplicação React de Gerenciamento de Projetos e Tarefas
+
+Projeto criado com as bibliotecas React JS e Material UI.
+
+## Explicaçao:
+
+Foi criado o arquivo/componente Modal.jsx como exemplo de edição para assim realizar o commit.
---
+++ src/pages/tarefa/Modal.jsx
@@ -0,0 +1,36 @@
+
+import React, { useState, useEffect } from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Button from '@mui/material/Button';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import Modal from '@mui/material/Modal';
+import CriarTarefa from './CriarTarefa';
+import EditarTarefa from './EditarTarefa';
+
+
+<Modal
+  open={open}
+  onClose={handleClose}
+  aria-labelledby="modal-modal-title"
+  aria-describedby="modal-modal-description"
+>
+  <Box sx={style}>
+    <Typography id="modal-modal-title" variant="h6" component="h2">
+      Prática Integradora - Módulo 10
+    </Typography>
+    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+      Texto de exemplo da descrição do modal.
+    </Typography>
+  </Box>
+</Modal>
